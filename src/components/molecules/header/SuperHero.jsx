0ca838e50a04adc80efb937e088073c9603bf43f
luisvilca10 <martinvilca@upeu.edu.pe>
2025-05-06 11:58:55 -0500

    SuperHeroç

--- src/components/molecules/header/SuperHero.jsx
+++ src/components/molecules/header/SuperHero.jsx
@@ -4,18 +4,18 @@ function SuperHero() {
 
   return (
     <>
-      <div className="2xl:w-1/3 xl:w-1/3 md:w-1/2 mx-auto lg:flex md:flex md:justify-between bg-red-700 text-white items-center lg:justify-center text-[0.625rem] md:text-xs px-4 py-1 sm:flex-none rounded-b-2xl transition-all duration-500">
+      <div className="2xl:w-1/3 xl:w-2/5 md:w-1/2 mx-auto lg:flex md:flex md:justify-between bg-red-700 text-white items-center lg:justify-center text-[0.625rem] md:text-xs px-4 py-1 sm:flex-none rounded-b-2xl transition-all duration-500">
         <ul className="flex space-x-4 justify-center flex-row">
           <li className="flex items-center gap-x-1">
-            <MapPinHouse size={"15px"} />
+            <MapPinHouse size={"10px"} />
             Arequipa - Yanahuara
           </li>
           <li className="flex items-center gap-x-1">
-            <PhoneCall size={"15px"} />
+            <PhoneCall size={"10px"} />
             +51 950 280 738
           </li>
           <li className="flex items-center gap-x-1">
-            <MailCheck size={"15px"} />
+            <MailCheck size={"10px"} />
             <a href="mailto:arequipacasacampo@gmail.com">arequipacasacampo@gmail.com</a>
           </li>
         </ul>
